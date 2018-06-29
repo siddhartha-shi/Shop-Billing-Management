db53a1b1cd7df6aa98a30568da124c6b2d9c160c
Siddhartha Sarkar <siddhartha.bd@gmail.com>
2018-06-29 16:58:07 +0600

    Copy Project

---
+++ main.cpp
@@ -0,0 +1,236 @@
+//By Siddhartha ( siddharth.sarker@yahoo.com )
+#include<iostream>
+#include<process.h>
+#include<fstream>
+#include <stdlib.h>
+using namespace std;
+
+class head
+{
+	char Iname[50][50];
+
+public:
+	int totalitems;
+	float Qty[3];
+	float price[3];
+	int vatprice[3];
+	int tprice[3];
+	void input();
+	void output();
+};
+class vat:public head
+{
+	float vats;
+public:
+	void vatcal();
+	void outputs();
+	void sum();
+};
+
+//******************************************************************
+//	    INPUT FUNCTION
+//******************************************************************
+
+void head::input()
+{
+	cout<<"How many items?= ";
+	cin>>totalitems;
+
+	for(int i=0; i<totalitems; i++)
+	{
+		cout<<"\nEnter name of item "<<i+1<<": ";
+		cin>>Iname[i];
+		cout<<"Enter quantity: ";
+		cin>>Qty[i];
+		cout<<"Enter price of item "<<i+1<<": ";
+		cin>>price[i];
+		tprice[i]=Qty[i]*price[i];
+	}
+}
+
+//******************************************************************
+//	   OUTPUT FUNCTION
+//******************************************************************
+
+
+
+void head::output()
+{
+	int a;
+
+	ifstream infile("COUNT.TXT");
+	infile>>a;
+
+	ofstream outfile("COUNT.TXT");
+	a+=1;
+	outfile<<a;
+	outfile.close();
+
+	{ofstream outfile("HIS.TXT", ios::app);
+	outfile<<endl<<"Bill No.: "<<a<<endl;
+	outfile<<"------------------------------------------------------------------------"<<endl;
+
+	cout<<"Name of item\tQuantity    Price   totalprice\n";
+	for(int i=0;i<totalitems;i++)
+	{
+		outfile<<"Name: "<<Iname[i]<<" Qty: "<<Qty[i]<<" Price: "<<tprice[i]<<endl;
+		cout<<Iname[i]<<"\t\t"<<Qty[i]<<"\t   "<<price[i]<<"\t   "<<tprice[i]<<'\n';
+	}
+
+	outfile<<"------------------------------------------------------------------------"<<endl;
+	outfile.close();
+	}
+}
+
+
+//******************************************************************
+//	   VAT CALCULATION
+//******************************************************************
+
+void vat::vatcal()
+{
+	input();
+	for(int i=0;i<totalitems;i++)
+	{
+		if(price[i]<=100.00)
+		{
+			vatprice[i]=tprice[i]+(0.03*tprice[i]);
+		}
+		else
+		{
+			vatprice[i]=tprice[i]+(0.1*tprice[i]);
+		}
+	}
+}
+//******************************************************************
+//	    VAT OUTPUTS
+//******************************************************************
+
+void vat::outputs()
+{
+	output();
+
+	float cash=0,sum=0,qty=0,sumt=0;
+
+	for(int i=0;i<totalitems;i++)
+	{
+	       sumt+=tprice[i];
+		   sum+=vatprice[i];
+		   qty+=Qty[i];
+	}
+	cout<<"Total:------------------------------------------------------------------------";
+	cout<<"\n\tQuantity= "<<qty<<"\t\t Sum= "<<sumt<<"\tWith Vat:"<<sum;
+	cout<<"\n******************************************************************************";
+
+pay:
+
+	cout<<"\nPayment summary\n";
+	cout<<"Total cash given: ";
+	cin>>cash;
+
+	if(cash>=sum)
+		cout<<"Total cash repaid: "<<cash-sum<<'\n';
+	else
+	{	cout<<"Cash given is less than total amount!!!";
+
+	goto pay;
+	}
+}
+
+
+//******************************************************************
+//	    PROTECTION PASSWORD
+//******************************************************************
+
+int passwords()
+{
+
+	char p1,p2,p3;
+
+	cout<<"\n\n\n\n\n\n\t\t\tENTER THE PASSWORD\n";
+
+	cin>>p1;
+	cout<<"*";
+	cin>>p2;
+	cout<<"*";
+	cin>>p3;
+	cout<<"*";
+
+	if ((p1=='s'||p1=='S')&&(p2=='i'||p2=='I')&&(p3=='d'||p3=='D'))
+
+		return 1;
+
+	else
+		return 0;
+}
+// END of Password.
+
+//****************************************************************
+//    	THE MAIN FUNCTION OF PROGRAM
+//****************************************************************
+
+
+int main()
+{
+	vat obj;
+	char opt, ch;
+	int a=1;
+	ifstream fin;
+
+	a==passwords();
+	if(!a)
+	{
+		for(int i=0;i<2;i++)
+		{
+			cout<<"\nWrong password try once more\n";
+			if(passwords())
+			{
+				goto last;
+			}
+			else
+			{
+				cout<<"\n\n\t\t\t all attempts failed.....";
+				cout<<"\n\n\n\t\t\t see you.................. ";
+				exit(0);
+			}
+
+		}
+		cout<<"\t\t\t sorry all attempts failed............. \n \t\t\tinactive";
+			 }
+	else{
+last:;
+
+
+	 do{
+start:
+	 cout<<"What you want to do?";
+	 cout<<"\n1.\tTo enter new entry\n2.\tTo view previous entries\n3.\tExit\n";
+	 cout<<"Enter your option: ";
+	 cin>>opt;
+	 switch(opt)
+	 {
+	 case'1':
+		 obj.vatcal();
+
+		 obj.outputs();
+		 goto start;
+	 case'2':
+
+		 fin.open("HIS.TXT", ios::in);
+		 while(fin.get(ch))
+		 {
+			 cout<<ch;
+		 }
+		 fin.close();
+
+		 goto start;
+	 case'3':
+		 exit(0);
+	 default:
+		 cout<<"\a";
+	 }
+
+	 }while(opt!=3);
+	}
+	return 0;
+}
